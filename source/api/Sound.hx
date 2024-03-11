08af5be4c4a18a190e139f5262ca0feeb43de915
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-03-11 20:37:06 +0700

    new sound function and variable

--- source/api/Sound.hx
+++ source/api/Sound.hx
@@ -6,9 +6,20 @@ class Sound
 {
 	public static function init()
 	{
+		GetLua.enter.addVar("toggleMuted", FlxG.sound.toggleMuted);
+		GetLua.enter.addVar("soundTrayEnabled", FlxG.sound.soundTrayEnabled);
+
 		GetLua.enter.addcallback("playSound", function(sound:String, ?loud:Float = 0.4, ?looped:Bool = false)
 		{
+			FlxG.sound.cache(Paths.sounds(sound));
 			FlxG.sound.play(Paths.sounds(sound), loud, looped);
+			return;
+		});
+
+		GetLua.enter.addcallback("playMusic", function(sound:String, ?loud:Float = 0.4, ?looped:Bool = false)
+		{
+			FlxG.sound.cache(Paths.music(sound));
+			FlxG.sound.playMusic(Paths.music(sound), loud, looped);
 			return;
 		});
 	}
