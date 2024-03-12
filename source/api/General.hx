dd0aa05ddac568fe5a35c9bf54caee152d57f546
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-03-12 21:31:25 +0700

    fixed getProperty

--- source/api/General.hx
+++ source/api/General.hx
@@ -6,33 +6,33 @@ class General
 	{
 		trace("general init!");
 
+		// fully copy from fnf psych engine
 		GetLua.enter.addcallback("getProperty", function(variables:String)
 		{
-			var arg:Array<String> = variables.split('.');
-			if (variables.length > 1)
+			var killMe:Array<String> = variables.split('.');
+			if (killMe.length > 1)
 			{
-				var dynathing:Dynamic;
-				if (PlayState.spritess.exists(arg[0]))
-					dynathing = PlayState.spritess.get(arg[0]);
-				else if (PlayState.textss.exists(arg[0]))
+				var coverMeInPiss:Dynamic = null;
+				if (PlayState.spritess.exists(killMe[0]))
 				{
-					dynathing = PlayState.textss.get(arg[0]);
+					coverMeInPiss = PlayState.spritess.get(killMe[0]);
+				}
+				else if (PlayState.textss.exists(killMe[0]))
+				{
+					coverMeInPiss = PlayState.textss.get(killMe[0]);
 				}
 				else
 				{
-					Reflect.getProperty(PlayState.init, variables);
-					return;
+					coverMeInPiss = Reflect.getProperty(PlayState.init, killMe[0]);
 				}
 
-				for (i in 1...arg.length - 1)
+				for (i in 1...killMe.length - 1)
 				{
-					dynathing = Reflect.getProperty(dynathing, arg[i]);
+					coverMeInPiss = Reflect.getProperty(coverMeInPiss, killMe[i]);
 				}
-				Reflect.getProperty(dynathing, arg[arg.length - 1]);
-				return;
+				return Reflect.getProperty(coverMeInPiss, killMe[killMe.length - 1]);
 			}
-			Reflect.getProperty(PlayState.init, variables);
-			return;
+			return Reflect.getProperty(PlayState.init, variables);
 		});
 
 		GetLua.enter.addcallback("setProperty", function(variables:String, value:Dynamic)
